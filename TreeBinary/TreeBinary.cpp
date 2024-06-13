14f6d4a4ee85cee7b2aab8cbdbc5c08ad15f8a0e
Aulianurfitria15 <aulianurfitria17@gmail.com>
2024-06-13 18:41:21 +0700

    membuat class binary tree

--- TreeBinary/TreeBinary.cpp
+++ TreeBinary/TreeBinary.cpp
@@ -18,3 +18,37 @@ public:
 	}
 };
 
+class BinaryTree
+{
+public:
+	Node* ROOT;
+
+	BinaryTree()
+	{
+		ROOT = nullptr;  //Initializing ROOT to null
+	}
+
+	void insert(string element) // Insert a node in the binary search tree
+	{
+		Node* newNode = new Node(element, nullptr, nullptr); // Allocate memory for the new node
+		newNode->info = element;  //Assign value to the data field of the new data
+		newNode->leftchild = nullptr;  // Make the left child of the new node point to NULL
+		newNode->rightchild = nullptr;  // Make the right child of the new data point to NULL
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode);  // Locate the node which be the parent of the node to be inserted
+
+		if (parent == nullptr)  // If the parent is NULL (Tree is empty)
+		{
+			ROOT = newNode;  // Mark the new node as ROOT
+			return;  //Exit
+		}
+
+		if (element < parent->info)  //If the value in the data field pf the new node is less than that of the parent
+		{
+			parent->leftchild = newNode;  //Make the left child of the parent point to the new node
+		}
+		else if (element > parent->info) // If the value in the data field of the new data is greater than that of the parent
+	}
+};
